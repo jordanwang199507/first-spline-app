3553cd6a95446c6592018d3d434189f918356c2b
Jordan Wang <>
2024-06-28 14:57:52 -0700

    completed first spline tutorial react app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import Spline from "@splinetool/react-spline";
+import styled from "styled-components";
+import Logo from "./assets/logo.svg";
+import Laptop from "./assets/icon-laptop.svg";
+import Twitter from "./assets/icon-twitter.svg";
+import Youtube from "./assets/icon-youtube.svg";
+import "./App.css";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className="wrapper">
+      <Spline
+        className="spline"
+        scene="https://prod.spline.design/TGf0NlfJeLiCky2X/scene.splinecode"
+      />
+      <div className="content">
+        <div className="menu">
+          <li>
+            <img src={Logo} alt="Logo" />
+          </li>
+          <li>
+            <a href="/">Home</a>
+          </li>
+          <li>
+            <a href="/">Download</a>
+          </li>
+          <li>
+            <a href="/">App</a>
+          </li>
+          <li>
+            <a href="/">Login</a>
+          </li>
+          <li>
+            <button>Get Started</button>
+          </li>
+        </div>
+        <h1>Collaborate with people</h1>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Bring your team together and build your community by using our
+          cross-platform app that lets you collaborate via chat, voice and by
+          sharing and storing unlimited media files. A world of topics is
+          waiting for you. Join the private beta.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <button>
+          <img src={Laptop} alt="Download" /> Download for Mac
+        </button>
+      </div>
+      <div className="social">
+        <div></div>
+        <img src={Twitter} alt="Twitter" />
+
+        <img src={Youtube} alt="Youtube" />
+      </div>
     </div>
   );
-}
+};
 
 export default App;
